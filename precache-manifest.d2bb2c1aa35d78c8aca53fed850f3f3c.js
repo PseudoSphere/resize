f49abfbdf74c9798c2b32cb72875e1c3e4356db9
Caleb Ferrell <Calebjf@gmail.com>
2019-07-21 12:48:58 -0700

    Updates

---
+++ precache-manifest.d2bb2c1aa35d78c8aca53fed850f3f3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bbf43c91d84f31c11a004392d06cf8",
+    "url": "/resize/index.html"
+  },
+  {
+    "revision": "7dad5cd91effe6488efc",
+    "url": "/resize/static/css/main.12bc82e8.chunk.css"
+  },
+  {
+    "revision": "e0eb240d4ad263d3fbbf",
+    "url": "/resize/static/js/2.cdc3d391.chunk.js"
+  },
+  {
+    "revision": "7dad5cd91effe6488efc",
+    "url": "/resize/static/js/main.90ec8445.chunk.js"
+  },
+  {
+    "revision": "fb3a9b2b053d8ac001ea",
+    "url": "/resize/static/js/runtime~main.34bfe465.js"
+  }
+]);
